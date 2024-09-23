debe7698b0a2e5d44a4456e2d3f5055408a84ea4
eric406-yeboah <ericusyeboah406@gmail.com>
2024-09-23 15:12:00 +0000

    auth

--- src/components/SignUpWithGoogle.jsx
+++ src/components/SignUpWithGoogle.jsx
@@ -60,7 +60,7 @@ const SignUpwithGoogle = () => {
   return (
     <div className="mt-6">
       <button
-        onClick={googleSignin}
+               onClick={() => window.location.href = ' https://fti-nexus-backend.onrender.com/api/v1/auth/signup/google'}
         type="button"
         className="w-full inline-flex items-center justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-500 bg-white hover:bg-gray-50"
         disabled={loading}  // Disable the button while loading
