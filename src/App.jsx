b4841acff08cae9085b28bc027289adad052ffc5
Ernest Frimpong <ernest.40frimpong@gmail.com>
2024-09-27 21:12:43 +0000

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import Login from "./pages/Login";
 import CreateAccount from "./pages/CreateAccount";
